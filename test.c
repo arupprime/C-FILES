9250381d54c5c478e3efdc89253ff3b6d3d63497
arupprime <arup.060101@gmail.com>
2020-04-02 20:50:07 +0530

    misc files

---
+++ test.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <string.h>
+#include <stddef.h>
+
+int main(){
+	int arr[100];
+	int positive=0, negative=0, zero=0;
+
+	printf("enter numbers:");
+
+	scanf("%d",arr);
+
+	for(int i=0; i<sizeof(arr)/sizeof(arr[0]); ++i){
+
+            if(arr[i]>0){
+                    positive+=1;
+            }
+            else if(arr[i]<0){
+                    negative+=1;
+            }
+            else if(arr[i]=0){
+                    zero+=1;
+            }
+	}
+
+	printf("%d",positive);
+	printf("%d",negative);
+	printf("%d",zero);
+	return 0;
+
+}
